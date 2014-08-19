e7526d1728557b45a2db45f7b275e11e6c06b805
A <A@art-pc>
2014-08-19 23:00:39 +0400

    Signed-off-by: A <A@art-pc>

---
+++ Spitter/src/main/java/com/akartkam/spitter/persistence/SpitterDao.java
@@ -0,0 +1,28 @@
+package com.akartkam.spitter.persistence;
+
+import java.util.List;
+
+import com.akartkam.spitter.domain.Spitter;
+import com.akartkam.spitter.domain.Spittle;
+
+public interface SpitterDao {
+  void addSpitter(Spitter spitter);
+
+  void saveSpitter(Spitter spitter);
+
+  Spitter getSpitterById(long id);
+
+  List<Spittle> getRecentSpittle();
+  
+  void saveSpittle(Spittle spittle);
+  
+  List<Spittle> getSpittlesForSpitter(Spitter spitter);
+
+  Spitter getSpitterByUsername(String username);
+  
+  void deleteSpittle(long id);
+  
+  Spittle getSpittleById(long id);
+  
+  List<Spitter> findAllSpitters();
+}
