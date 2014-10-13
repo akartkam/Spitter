4f528e49b574fc5144327ab557902ed021f89277
Artur Akchurin <akartkam@gmail.com>
2014-10-13 18:31:29 +0400

    Signed-off-by: Artur Akchurin <akartkam@gmail.com>

---
+++ Spitter/src/main/java/com/akartkam/spitter/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.akartkam.spitter.controller;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+  @RequestMapping(method=GET)
+  public void showLoginForm() { }
+}
